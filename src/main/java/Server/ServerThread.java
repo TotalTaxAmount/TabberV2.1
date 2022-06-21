92c4544e6c839c60247cb8d69396366b0c535d56
shiel <shieldscoen@gmail.com>
2022-06-21 14:09:09 -0600

    Initial commit

---
+++ src/main/java/Server/ServerThread.java
@@ -0,0 +1,44 @@
+package Server;
+
+import java.io.*;
+import java.net.Socket;
+
+public class ServerThread extends Thread {
+    protected Socket socket;
+
+    public ServerThread(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        InputStream inp = null;
+        BufferedReader brinp = null;
+        DataOutputStream out = null;
+        try {
+            inp = socket.getInputStream();
+            brinp = new BufferedReader(new InputStreamReader(inp));
+            out = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            System.out.println("Error creating thread for: " + socket.getInetAddress());
+            return;
+        }
+
+        String line;
+        while (true) {
+            try {
+                line = brinp.readLine();
+                if ((line == null) || line.equalsIgnoreCase("QUIT")) {
+                    socket.close();
+                    return;
+                } else {
+                    out.writeBytes(line + "\n\r");
+                    out.flush();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+        }
+    }
+}
---
+++ src/main/java/Server/Sever.java
@@ -0,0 +1,56 @@
+package Server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Sever {
+
+    private static final int port = 4765;
+    private static ArrayList<ServerThread> clients = new ArrayList<>();
+
+    public static ServerSocket serverSocket = null;
+
+    public static Socket socket = null;
+
+    public static void main(String args[]) {
+
+
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            System.out.println("Error starting server socket: ");
+            e.printStackTrace();
+            return;
+        }
+
+        while (true) {
+//            Scanner reader = new Scanner(System.in);
+//            System.out.println("Enter command");
+//            System.out.print("> ");
+//
+//            String cmd = reader.next();
+            new Thread(() -> {
+                try {
+                    socket = serverSocket.accept();
+
+                    ServerThread client = new ServerThread(socket);
+
+                    if (clients.size() == 0) {
+                        client.start();
+                    }
+                    clients.add(client);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+
+            System.out.println(clients);
+
+        }
+    }
+
+}
